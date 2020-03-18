53f7da53784b3c6440d3c6e7145f9bd7a019aa9a
sjrmac <sjrmac@gmail.com>
2020-03-17 21:59:48 -0700

    frt: clean up BoardConfig.mk and move include some essentials
    Added in stuff from kernel.mk and twrpbuilder generic tree to clean up and reduce outside dependencies. Still needs further work
    
    Signed-off-by: sjrmac <sjrmac@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,29 +16,52 @@
 
 LOCAL_PATH := device/nokia/frt
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+
+# Platform
 TARGET_BOARD_PLATFORM := mt6737m
+
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := frt
 
-# Recovery
-TARGET_USERIMAGES_USE_EXT4 := true
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 buildvariant=user
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x04000000 --tags_offset 0x0e000000
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/zImage-dtb
+
+# Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 BOARD_FLASH_BLOCK_SIZE := 0
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Recovery
 #BOARD_HAS_NO_REAL_SDCARD := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_RECOVERY_SWIPE := true
 BOARD_USES_MMCUTILS := true
 BOARD_SUPPRESS_EMMC_WIPE := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/f_mass_storage/lun/file
+
+# TWRP
 TW_EXCLUDE_SUPERSU := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_INCLUDE_CRYPTO := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/f_mass_storage/lun/file
 TW_MTP_DEVICE := /dev/mtp_usb
 # The path to a temperature sensor
 TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
-
-include $(LOCAL_PATH)/kernel.mk
-include device/generic/twrpbuilder/BoardConfig32.mk
-
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
